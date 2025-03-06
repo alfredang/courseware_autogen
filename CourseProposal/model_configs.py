6be116a18c14e823af586f336b3323ff5330f6ee
macus123 <122253505+macus123@users.noreply.github.com>
2025-03-06 11:11:08 +0800

    Update model_configs.py

--- CourseProposal/model_configs.py
+++ CourseProposal/model_configs.py
@@ -4,12 +4,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-# OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-# DEEPSEEK_API_KEY = st.secrets["DEEPSEEK_API_KEY"]
-# GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+# DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
+# GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+DEEPSEEK_API_KEY = st.secrets["DEEPSEEK_API_KEY"]
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 
 # GPT-4o Mini (Default)
 default_config = {
